ef9f81560ad00fc27786ac03a9fa47068841877d
Christian-GM <christian@gmeiner.org>
2015-05-13 15:09:45 +0200

    no message

--- ScOREO/client/app/main/main.controller.js
+++ ScOREO/client/app/main/main.controller.js
@@ -1,23 +1,34 @@
 'use strict';
 
 angular.module('softwareEngineeringApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http, socket, Auth) {
     $scope.awesomeThings = [];
+    $scope.studentenInfos = {
+      vName: String,
+      nName: String,
+      gebDatum: Date,
+      eMail: String,
+      Vorlesungen: []
+    };
 
-    $http.get('/api/things').success(function(awesomeThings) {
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $http.get('/api/things').success(function (awesomeThings) {
       $scope.awesomeThings = awesomeThings;
       socket.syncUpdates('thing', $scope.awesomeThings);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
+    $scope.addThing = function () {
+      if ($scope.newThing === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
+      $http.post('/api/things', {
+        name: $scope.newThing
+      });
       $scope.newThing = '';
     };
 
-    $scope.deleteThing = function(thing) {
+    $scope.deleteThing = function (thing) {
       $http.delete('/api/things/' + thing._id);
     };
 
